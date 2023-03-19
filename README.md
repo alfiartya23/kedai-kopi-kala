cef6a69f1d70a657f9459ea6189899f2fdbef291
alfiartya23 <alfiartya.diwanda@gmail.com>
2023-03-19 21:54:41 +0700

    Update Readme and CSS & add Sidebar Menu Logic

--- README.md
+++ README.md
@@ -1,4 +1,19 @@
 # kedai-kopi-kala
 
-Website ini dibuat sebagai latihan untuk pembuatan website kedai kopi sederhana dari video tutorial di channel Web Programming UNPAS.
-Source: https://youtu.be/MCVkMmYL-aY
+## Selamat datang di repository Kedai Kopi Kala.
+
+Website ini dibuat sebagai latihan untuk pembuatan landing page Kedai Kopi Kala dari video tutorial di channel Web Programming UNPAS.
+
+## Teknologi
+
+Teknologi yang saya gunakan:
+
+- HTML
+- CSS
+- JavaScript
+
+## Sumber
+
+Youtube Link: (https://youtu.be/MCVkMmYL-aY)
+
+Terima kasih 🙌
--- script/script.js
+++ script/script.js
@@ -4,21 +4,16 @@
 const navBarNav = document.querySelector(".navbar-nav");
 const hamburgerMenu = document.querySelector("#hamburger-menu");
 
+// LOGIC UNTUK KLIK HAMBURGER MENU
 hamburgerMenu.addEventListener("click", function () {
-  // TOGGLE BISA UNTUK MEMBUKA DAN TUTUP
-  // navBarNav.classList.toggle("active");
-  // ADD HANYA UNTUK MEMBUKA
-  navBarNav.classList.add("active");
-  navBarNav.classList.add("overlay");
+  navBarNav.classList.toggle("active");
 });
 
-// KLIK DILUAR SIDEBAR ATAU DIMANAPUN DI HALAMAN
-// UNTUK MENGHILANGKAN MENU SIDEBAR
+// KLIK DILUAR SIDEBAR ATAU DIMANAPUN DI HALAMAN UNTUK MENGHILANGKAN MENU SIDEBAR
 document.addEventListener("click", function (e) {
   // SELAMA YANG DIKLIK BUKAN NAVBAR DAN BUKAN HAMBURGER MENU
   if (!hamburgerMenu.contains(e.target) && !navBarNav.contains(e.target)) {
     // HILANGKAN CLASS ACTIVE
     navBarNav.classList.remove("active");
-    navBarNav.classList.remove("overlay");
   }
 });
